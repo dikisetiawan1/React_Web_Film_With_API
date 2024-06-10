0b1966ab85e20da35e53f113aed682bbecf19197
Diki Setiawan <dikisetia101@gmail.com>
2024-06-10 23:29:02 +0700

    upload source code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import {getMovieList, searchMovie} from "./api"
 import './App.css';
 
 function App() {
+  const [popularMovies, setPopularMovies] = useState([])
+
+useEffect(() => {
+  getMovieList().then((result) => {
+    setPopularMovies(result)
+  })
+}, [] )
+
+
+const PopularMovieList = () => {
+  return popularMovies.map((movie, i) => {
+    return (
+    <div className="Movie-wrapper" key={i}>
+    <div className="Movie-title">{movie.title}</div>
+    <img src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} alt="" className='Movie-image' />
+    <div className="Movie-date">Release : {movie.release_date}</div>
+    <div className="Movie-rate">{movie.vote_average}</div>
+  </div>
+    )
+  })
+}
+
+
+const search = async (q) =>{
+if (q.length > 3){
+  const query = await searchMovie(q)
+  setPopularMovies(query.results)
+}
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <header className='app-header'>
+    <h1>GET MOVIE WITH API</h1>
+        <input 
+        placeholder='cari film disini' 
+        className='Movie-search'
+        onChange={({target}) => search(target.value)} />
+        <div className="Movie-container">
+        <PopularMovieList/>
+        </div>
+   </header>
+      </div>
+
   );
 }
 
